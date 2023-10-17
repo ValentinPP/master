2b43191464e4b8e81f75fd135b9965180a999385
ValentinPaun <paun.valentin@gmail.com>
2023-10-17 21:36:44 +0300

    Fist push

--- src/com/itFactory/packageExercitiu1/Class1.java
+++ src/com/itFactory/packageExercitiu1/Class1.java
@@ -3,61 +3,32 @@ package com.itFactory.packageExercitiu1;
 public class Class1 {
     public static void main (String [] args)
     {
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
-        System.out.println("Prima linie");
+        System.out.println("First line");
+        System.out.println("Line 2");
+        System.out.println("Line 3");
+        System.out.println("Line 4");
+        System.out.println("Line 5");
+        System.out.println("Line 6");
+        System.out.println("Line 7");
+        System.out.println("Line 8");
+        System.out.println("Line 9");
+        System.out.println("Line 10");
+        System.out.println("Line 11");
+        System.out.println("Line 12");
+        System.out.println("Line 13");
+        System.out.println("Line 14");
+        System.out.println("Line 15");
+        System.out.println("Line 16");
+        System.out.println("Line 17");
+        System.out.println("Line 18");
+        System.out.println("Line 19");
+        System.out.println("Line 20");
 
+        for ( int i = 1; i <= 21; i++) {
+        System.out.println("Line number: " + i);
+        System.out.print("Consecutive line number: " + i  + " ");
     }
 
 
-//    public void (String [] args)
-//    {
-//        System.out.println("Linia 2");
-//    }
-////    public static void main (String [] args)
-//    {
-//        System.out.println("Prima linie");
-//    }
-//    public static void main (String [] args)
-//    {
-//        System.out.println("Prima linie");
-//    }
-//    public static void main (String [] args)
-//    {
-//        System.out.println("Prima linie");
-//    }
-//    public static void main (String [] args)
-//    {
-//        System.out.println("Prima linie");
-//    }
-//    public static void main (String [] args)
-//    {
-//        System.out.println("Prima linie");
-//    }
-//    public static void main (String [] args)
-//    {
-//        System.out.println("Prima linie");
-//    }
+    }
 }
